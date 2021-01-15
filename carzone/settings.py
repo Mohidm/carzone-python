18ae13fcafe7bc46fdd120777420e7a05933e5e9
Mohid <mohidm94@gmail.com>
2021-01-15 11:36:14 +0530

    secret key disabled

--- carzone/settings.py
+++ carzone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g6=gdohnsmzqw$4e=#(22$woc=)vp^ok4dowzs7d^wgp(#$f5+'
+# SECRET_KEY = 'g6=gdohnsmzqw$4e=#(22$woc=)vp^ok4dowzs7d^wgp(#$f5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
