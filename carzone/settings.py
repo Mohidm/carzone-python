59bd3f649e4beb7b277ec697400c00dd4739e8ba
Mohid <mohidm94@gmail.com>
2021-01-17 10:31:43 +0530

    created html pages

--- carzone/settings.py
+++ carzone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'g6=gdohnsmzqw$4e=#(22$woc=)vp^ok4dowzs7d^wgp(#$f5+'
+SECRET_KEY = 'g6=gdohnsmzqw$4e=#(22$woc=)vp^ok4dowzs7d^wgp(#$f5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "pages/static"),
+]
